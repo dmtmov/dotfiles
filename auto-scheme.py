db7c4122d0d5ccf1fa13793eaacba4fead0c839a
Dmitry Movchan <mov.deamon@gmail.com>
2020-04-14 18:08:46 +0300

    update ST3 as well. solarized only

--- auto-scheme.py
+++ auto-scheme.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3.7
 
 import iterm2
+import os
+
 
 async def main(connection):
     async with iterm2.VariableMonitor(connection, iterm2.VariableScopes.APP, "effectiveTheme", None) as mon:
@@ -12,8 +14,11 @@ async def main(connection):
             parts = theme.split(" ")
             if "dark" in parts:
                 preset = await iterm2.ColorPreset.async_get(connection, "Solarized Dark")
+                os.system("pushd /Users/d/Library/Application\ Support/Sublime\ Text\ 3/Packages/User; sed 's/light/dark/' ./Preferences.sublime-settings > settings_new; mv settings_new ./Preferences.sublime-settings; popd;")
+                
             else:
                 preset = await iterm2.ColorPreset.async_get(connection, "Solarized Light")
+                os.system("pushd /Users/d/Library/Application\ Support/Sublime\ Text\ 3/Packages/User; sed 's/dark/light/' ./Preferences.sublime-settings > settings_new; mv settings_new ./Preferences.sublime-settings; popd;")
 
             # Update the list of all profiles and iterate over them.
             profiles=await iterm2.PartialProfile.async_query(connection)
