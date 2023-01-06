0e15734524c868b3ef0f1a2e2043bd5b856d6f76
Dmytro Movchan <dmytro.movchan@welltech.com>
2023-01-06 22:39:11 +0200

    add gutter icon more space

--- .config/nvim/lua/configs/completion.lua
+++ .config/nvim/lua/configs/completion.lua
@@ -108,8 +108,8 @@ local sign = function(opts)
   })
 end
 
-sign({name = 'DiagnosticSignError', text = '✘'})
-sign({name = 'DiagnosticSignWarn', text = '▲'})
-sign({name = 'DiagnosticSignHint', text = '⚑'})
-sign({name = 'DiagnosticSignInfo', text = '⇲'})
+sign({name = 'DiagnosticSignError', text = ' ✘'})
+sign({name = 'DiagnosticSignWarn', text = ' ▲'})
+sign({name = 'DiagnosticSignHint', text = ' ⚑'})
+sign({name = 'DiagnosticSignInfo', text = ' ⇲'})
 
